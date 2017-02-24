e297a945692c656fd4c729854c2976c0e02fd9b1
Xi Xia <triplextension@gmail.com>
2017-02-24 09:45:35 -0800

    all numbers convert and work. working on styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,56 @@
+var resultArray = [];
+var makeNumberList = function (number) {
+  resultArray=[];
+  for (var i=1; i<=number; i++) {
+  resultArray.push(i);
+  }
+};
+
+var displayNumberList = function (list) {
+    $("#results").empty();
+  for (var j=0; j<list.length; j++){
+
+    var numberToScreen = list[j];
+    var originalNumber = '<span class="original">'+j+"</span>"
+    console.log(numberToScreen);
+  $("#results").append("<h2>"+originalNumber+" "+numberToScreen+"<h2>");
+  };
+};
+
+var find15 = function (){
+  for (var j=0; j<resultArray.length; j++){
+    if (Number.isInteger(resultArray[j]/15)) {
+      resultArray.splice(j,1,"ping pong");
+    }
+  };
+};
+
+var find5 = function (){
+  for (var j=0; j<resultArray.length; j++){
+    if (Number.isInteger(resultArray[j]/5)) {
+      resultArray.splice(j,1,"pong");
+    }
+  };
+};
+
+var find3 = function (){
+  for (var j=0; j<resultArray.length; j++){
+    if (Number.isInteger(resultArray[j]/3)) {
+      resultArray.splice(j,1,"ping");
+    }
+  };
+};
+
+
 $(document).ready(function (){
   $("#numberList").submit(function(event) {
-    $("#results").empty();
     event.preventDefault();
-    var inputNumberList = parseInt($("input#numbers").val());
-    $("#results").append("<h1>"+inputNumberList+"</h1>");
+    var inputNumber = parseInt($("input#numbers").val());
+    makeNumberList(inputNumber);
+    find15();
+    find5();
+    find3();
+    displayNumberList(resultArray);
     $("#numberList").trigger("reset");
   });
-
 });
