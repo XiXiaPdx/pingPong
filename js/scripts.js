dd93ebc3b7a87cd2dd9b74c6ca7418057c338ee2
Xi Xia <triplextension@gmail.com>
2017-02-24 08:36:03 -0800

    add form reset, display number, remove previous number on each submit

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,10 @@
 $(document).ready(function (){
+  $("#numberList").submit(function(event) {
+    $("#results").empty();
+    event.preventDefault();
+    var inputNumberList = parseInt($("input#numbers").val());
+    $("#results").append("<h1>"+inputNumberList+"</h1>");
+    $("#numberList").trigger("reset");
+  });
 
 });
